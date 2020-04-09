038cc958307022c9eca0c5a292043cc65a85af60
Anna-Luise Lorenz <annaluise.lorenz@gmail.com>
2020-04-09 17:31:29 +0200

    Clean Up Menu WORKS

--- javascript/mainFunctions.js
+++ javascript/mainFunctions.js
@@ -1,26 +1,3 @@
-var home = document.getElementById("home");
-var works = document.getElementById("works");
-var writing = document.getElementById("writing");
-var teaching = document.getElementById("teaching");
-var info = document.getElementById("info");
-var menuBar = document.getElementsByClassName("menuitemContainer1");
-
-
-
-home.addEventListener("mouseover", mouseOver);
-// menubar.addEventListener("mouseout", mouseOut);
-
-function mouseOver() {
-    console.log("over");
-    document.getElementById("projectsHome").style.display = "block";
-
-}
-
-function mouseOut() {
-    console.log("out");
-    document.getElementById("projectsHome").style.display = "none";
-}
-
 $('#nav > li').each(function(){
        var t = null;
        var li = $(this);
@@ -28,14 +5,14 @@ $('#nav > li').each(function(){
            t = setTimeout(function(){
                li.find("ul").slideDown(300);
                t = null;
-           }, 300);
+           }, 100);
        }, function(){
            if (t){
                clearTimeout(t);
                t = null;
            }
            else
-               li.find("ul").slideUp(200);
+               li.find("ul").slideUp(300);
        });
    });
 
