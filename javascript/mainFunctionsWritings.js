2e1b227f0a2a14a3ae440ecc5c5558d7768698ef
Anna-Luise Lorenz <annaluise.lorenz@gmail.com>
2020-04-19 01:18:08 +0200

    general: introduce rotating circles (buggy!) general: fix menu index.html: projectname in image

--- javascript/mainFunctionsWritings.js
+++ javascript/mainFunctionsWritings.js
@@ -83,7 +83,7 @@ var diameterCircle = 200;
 
 // ------------------------------------------------------------------------------------------------------------
 
-var keywords=['info', 'idealism']; //,'idealism1','capitalism2','body3','xeno4'
+var keywords=['info']; //,'idealism1','capitalism2','body3','xeno4'
 var keywordsURL=['www.anaconda.cc/reality', 'www.anaconda.cc/idealism', 'www.anaconda.cc/capitalism', 'www.anaconda.cc/body', 'www.anaconda.cc/xeno']
 var projectlist = ['The Great Encounter, The Ending', 'The Young Planet', '04PP', 'Shush Tones', 'Berlin Netzkunst', 'Next Project', 'TongueTongue', 'The Great Encounter, The Ending', 'The Young Planet', '04PP', 'Shush Tones', 'Berlin Netzkunst', 'Next Project', 'TongueTongue']
 
@@ -430,136 +430,129 @@ function createProjectlist() {
 
 // ----------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------- CIRCLES START
-
-
-class KeywordCircle {
-    constructor(initialX,initialY){
-        this.initialX = initialX;
-        this.initialY = initialY;
-    }
-}
-
-function generateCirclePosition() { // 1.1
-    var grain = 100;
-    const randomPosX = Math.floor(Math.random() * grain);
-    const randomPosY = Math.floor(Math.random() * grain);
-    const scaleX = (randomPosX, in_min, in_max, out_min, out_max) => {
-    return (randomPosX - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
-    const scaleY = (randomPosY, in_min, in_max, out_min, out_max) => {
-    return (randomPosY - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
-    initialX = scaleX(randomPosX,0, grain, 0, (window.innerWidth-diameterCircle));
-    initialY = scaleY(randomPosY,0, grain, 0, (window.innerHeight-diameterCircle));
-}
-
-function createInitialCircles() { // 1.2
-    for(var i=0;i<keywords.length;i++){ //generates 5 initial circles
-        generateCirclePosition();
-        circle = new KeywordCircle(initialX,initialY);
-        circleObjectArr.push(circle); // Array mit allen KeywordCircle Instanzen
-        circleObjectArrPosY.push(circleObjectArr[i].initialY); // Array mit allen Keyword Instanzen y-Werten
-    }
-    // console.log("circleObjectArr: " + circleObjectArr);
-    console.log("circleObjectArrPosY: " + circleObjectArrPosY);
-}
-
-
-function updateCirclePos() {
-    var step = (new Date()).getTime() - startTime; // zählt als counter hoch
-    step *= (-0.01);
-
-    // for(var i=0;i<5;i++){
-    // }
-    // console.log("steppArr: " + stepArr); funktioniert
-    function bla() {
-      for(var i=0;i<5;i++){ //circleObjectArr verändert sich innerhalb der for Schleife, aber nicht außerhalb....
-        stepArr[i]=step;
-        startTimeArr[i] = startTime;
-        circlePosUpdate = circleObjectArrPosY[i] + stepArr[i];
-        circleObjectArr[i].initialY = circlePosUpdate;
-        $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
-
-          // if (circleObjectArr[i].initialY < 50) {
-          //   circleObjectArrPosY[i] = 500;
-          //
-          //   startTime[i] = +(new Date()); // reset Starttime
-          //   updateCirclePos();
-          //   //   // generateCirclePosition();
-          //   //   circleObjectArr[i].initialY = 200;
-          //   //   console.log(circleObjectArr[i].initialY);
-          //   //   // $(".circle" + i).css("left", circleObjectArr[i].initialX);
-          //   //   $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
-          //   //   // updateCirclePos(); // starte updateCircle von Anfang an
-          // }
-          // REBIRTH NEW CIRCLE
-          // if (circleObjectArr[i].initialY < 50) {
-
-
-            // updateCirclepos?
-            // generateCirclePosition();
-            // circle = new KeywordCircle(initialX,initialY);
-            // circleObjectArr.push(circle);
-            // $(".circle" + i).css("left", circleObjectArr[i].initialX);
-            // $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
-            // circleObjectArrPosY.push(circleObjectArr[i].initialY);
-            //
-            // console.log(i);
-            // console.log("davor: "+circleObjectArrPosY[i]);
-            // console.log(circleArrPos[i].initialY);
-            // circleObjectArrPosY[i] = window.innerHeight-100;//array updatet sich nicht
-            // circleObjectArr[i].initialY = circleObjectArrPosY[i];
-            // console.log("danach: "+circleObjectArrPosY[i]);
-            // console.log(circleObjectArr[i].initialY);
-            // debugger;
-          // }
-
-        }
-    }
-    requestAnimationFrame(updateCirclePos);
-
-}
-
-function drawFirstCircles() { // 2
-  // createInitialCircles(); //raus und stattdessen in document.ready function drinlassen?
-    for(var i=0;i<5;i++){
-        $wrapper = $('<div />').appendTo('body');
-        $wrapper.attr('class', 'circleAnimation');
-        $div = $('<div />').appendTo($wrapper);
-        $p = $('<p />').appendTo($div);
-        $a = $('<a />').appendTo($p);
-        $a.attr('href', keywordsURL[i]);
-        $a.text( keywords[i] );
-        $p.attr('class', 'textKeywords');
-        $div.attr('class', 'circle'+ i);
-        // rotation
-        var rotation1 = 1;
-        var rotation2 = 2;
-        var rotation3 = 3;
-        var randomIdCircle = Math.random() < 0.5 ? rotation1 : (Math.random() < 0.5 ? rotation2 : rotation3);
-        $div.attr('id', 'rotationCircle'+ randomIdCircle);
-        // position (kann später mit updateCircles raus?)
-        $(".circle" + i).css("left", circleObjectArr[i].initialX);
-        $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
-    }
-
-    $('.aboutIcon').css("left", initialX);
-    $('.aboutIcon').css("top", initialY);
-    $('.aboutIcon').attr('id', 'rotationCircle2');
-
-    // updateCirclePos(); //raus und stattdessen in document.ready function drinlassen?
-  }
-
-// ---------------------------------------------------------------------------- CIRCLES END
-
+// class KeywordCircle {
+//     constructor(initialX,initialY){
+//         this.initialX = initialX;
+//         this.initialY = initialY;
+//     }
+// }
+//
+// function generateCirclePosition() { // 1.1
+//     var grain = 100;
+//     const randomPosX = Math.floor(Math.random() * grain);
+//     const randomPosY = Math.floor(Math.random() * grain);
+//     const scaleX = (randomPosX, in_min, in_max, out_min, out_max) => {
+//     return (randomPosX - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+//     }
+//     const scaleY = (randomPosY, in_min, in_max, out_min, out_max) => {
+//     return (randomPosY - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+//     }
+//     initialX = scaleX(randomPosX,0, grain, 0, (window.innerWidth-diameterCircle));
+//     initialY = scaleY(randomPosY,0, grain, 0, (window.innerHeight-diameterCircle));
+// }
+//
+// function createInitialCircles() { // 1.2
+//     for(var i=0;i<keywords.length;i++){ //generates 5 initial circles
+//         generateCirclePosition();
+//         circle = new KeywordCircle(initialX,initialY);
+//         circleObjectArr.push(circle); // Array mit allen KeywordCircle Instanzen
+//         circleObjectArrPosY.push(circleObjectArr[i].initialY); // Array mit allen Keyword Instanzen y-Werten
+//     }
+//     // console.log("circleObjectArr: " + circleObjectArr);
+//     console.log("circleObjectArrPosY: " + circleObjectArrPosY);
+// }
+//
+//
+// function updateCirclePos() {
+//     var step = (new Date()).getTime() - startTime; // zählt als counter hoch
+//     step *= (-0.01);
+//
+//     // for(var i=0;i<5;i++){
+//     // }
+//     // console.log("steppArr: " + stepArr); funktioniert
+//     function bla() {
+//       for(var i=0;i<5;i++){ //circleObjectArr verändert sich innerhalb der for Schleife, aber nicht außerhalb....
+//         stepArr[i]=step;
+//         startTimeArr[i] = startTime;
+//         circlePosUpdate = circleObjectArrPosY[i] + stepArr[i];
+//         circleObjectArr[i].initialY = circlePosUpdate;
+//         $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
+//
+//           // if (circleObjectArr[i].initialY < 50) {
+//           //   circleObjectArrPosY[i] = 500;
+//           //
+//           //   startTime[i] = +(new Date()); // reset Starttime
+//           //   updateCirclePos();
+//           //   //   // generateCirclePosition();
+//           //   //   circleObjectArr[i].initialY = 200;
+//           //   //   console.log(circleObjectArr[i].initialY);
+//           //   //   // $(".circle" + i).css("left", circleObjectArr[i].initialX);
+//           //   //   $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
+//           //   //   // updateCirclePos(); // starte updateCircle von Anfang an
+//           // }
+//           // REBIRTH NEW CIRCLE
+//           // if (circleObjectArr[i].initialY < 50) {
+//
+//
+//             // updateCirclepos?
+//             // generateCirclePosition();
+//             // circle = new KeywordCircle(initialX,initialY);
+//             // circleObjectArr.push(circle);
+//             // $(".circle" + i).css("left", circleObjectArr[i].initialX);
+//             // $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
+//             // circleObjectArrPosY.push(circleObjectArr[i].initialY);
+//             //
+//             // console.log(i);
+//             // console.log("davor: "+circleObjectArrPosY[i]);
+//             // console.log(circleArrPos[i].initialY);
+//             // circleObjectArrPosY[i] = window.innerHeight-100;//array updatet sich nicht
+//             // circleObjectArr[i].initialY = circleObjectArrPosY[i];
+//             // console.log("danach: "+circleObjectArrPosY[i]);
+//             // console.log(circleObjectArr[i].initialY);
+//             // debugger;
+//           // }
+//
+//         }
+//     }
+//     requestAnimationFrame(updateCirclePos);
+//
+// }
+//
+// function drawFirstCircles() { // 2
+//   // createInitialCircles(); //raus und stattdessen in document.ready function drinlassen?
+//     for(var i=0;i<5;i++){
+//         $wrapper = $('<div />').appendTo('body');
+//         $wrapper.attr('class', 'circleAnimation');
+//         $div = $('<div />').appendTo($wrapper);
+//         $p = $('<p />').appendTo($div);
+//         $a = $('<a />').appendTo($p);
+//         $a.attr('href', keywordsURL[i]);
+//         $a.text( keywords[i] );
+//         $p.attr('class', 'textKeywords');
+//         $div.attr('class', 'circle'+ i);
+//         // rotation
+//         var rotation1 = 1;
+//         var rotation2 = 2;
+//         var rotation3 = 3;
+//         var randomIdCircle = Math.random() < 0.5 ? rotation1 : (Math.random() < 0.5 ? rotation2 : rotation3);
+//         $div.attr('id', 'rotationCircle'+ randomIdCircle);
+//         // position (kann später mit updateCircles raus?)
+//         $(".circle" + i).css("left", circleObjectArr[i].initialX);
+//         $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
+//     }
+//
+//     $('.aboutIcon').css("left", initialX);
+//     $('.aboutIcon').css("top", initialY);
+//     $('.aboutIcon').attr('id', 'rotationCircle2');
+//
+//     // updateCirclePos(); //raus und stattdessen in document.ready function drinlassen?
+//   }
 
  $(document).ready(function(){
      createProjectlist();
-     // DRAW CIRCLES
-     createInitialCircles();
-     drawFirstCircles();
-     updateCirclePos();
+     // createInitialCircles();
+     // drawFirstCircles();
+     // updateCirclePos();
  });
 
 
