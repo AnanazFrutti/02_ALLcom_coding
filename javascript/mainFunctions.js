fd6954c52e2bebfd3d4461650fdcc36c0640fa4f
Anna-Luise Lorenz <annaluise.lorenz@gmail.com>
2020-04-18 14:18:46 +0200

    jump to anchor on new site

--- javascript/mainFunctions.js
+++ javascript/mainFunctions.js
@@ -42,10 +42,51 @@ $('#nav > li').each(function(){
              top: offsetPosition,
              behavior: 'smooth'
            });
-
        });
-
    });
+
+// jump to anchor on new site
+var jump=function(e)
+   {
+      if (e){
+          e.preventDefault();
+          var target = $(this).attr("href");
+          console.log("1");
+      }else{
+          var target = location.hash;
+          console.log("2");
+      }
+
+      $('html,body').animate(
+      {
+          scrollTop: $(target).offset().top
+      },2000,function()
+      {
+          location.hash = target;
+      });
+
+   }
+
+$('html, body').hide();
+
+$(document).ready(function()
+{
+
+    $('a[href^=\\#]').on("click", jump); // find all href elements starting with #, and execute jump function click event handler the jump function to it
+
+    if (location.hash){
+        setTimeout(function(){
+            $('html, body').scrollTop(0).show();
+            jump();
+            console.log("3");
+        }, 0);
+    }else{
+        $('html, body').show();
+        console.log("4");
+    }
+});
+
+// jump to anchor on new site END
 
 
 (function() {
