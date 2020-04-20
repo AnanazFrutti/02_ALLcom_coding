f57597048d8920968b4dbfdd82104a36b8422ea9
Anna-Luise Lorenz <annaluise.lorenz@gmail.com>
2020-04-20 14:31:47 +0200

    fill circles according to contents of keywords

--- javascript/mainFunctions.js
+++ javascript/mainFunctions.js
@@ -1,3 +1,52 @@
+var diameterCircle = 200;
+
+var keywords=['info', 'idealism', 'capitalism']; //,'idealism1','capitalism2','body3','xeno4'
+var keywordsURL=['www.anaconda.cc/reality', 'www.anaconda.cc/idealism', 'www.anaconda.cc/capitalism', 'www.anaconda.cc/body', 'www.anaconda.cc/xeno']
+var projectlist = ['The Great Encounter, The Ending', 'The Young Planet', '04PP', 'Shush Tones', 'Berlin Netzkunst', 'Next Project', 'TongueTongue', 'The Great Encounter, The Ending', 'The Young Planet', '04PP', 'Shush Tones', 'Berlin Netzkunst', 'Next Project', 'TongueTongue']
+
+var $wrapper;
+var $div;
+var $p;
+var $a;
+var randomPosX;
+var randomPosY;
+var yyy;
+var initialPositionX;
+var initialPositionY;
+var circleArr = new Array();
+
+
+var startTime = +(new Date());
+var startTimeArr = new Array(5);
+
+var circle;
+
+var circleObjectArr = new Array(); // Object mit allen Instanzen
+var circleObjectArrPosY = new Array(); // aus Object herausdistillierte y-Werte
+var stepArr = new Array(5);
+let start = null;
+
+var initialX;
+var initialY;
+
+var circlePosUpdate;
+
+var slideIndex = 1;
+var closeSlideshow = false;
+var counter = 0;
+var j = 1;
+
+var currentParent;
+var currentParentHover;
+
+var exampleDestination = document.querySelector('.projecttitle1');
+
+var initialSpanWrapperWidthArr = new Array();
+var spanWrapperWidth;
+var updateSpanWrapperWidthArr;
+
+var element = document.body;
+
 // ---------------------------------------------------- Dropdown menu on hover
 
 $('#nav > li').each(function(){
@@ -76,53 +125,6 @@ $(document).ready(function()
 });
 
 // ---------------------------------------------------- jump to anchor on new site END
-
-var cvIsVisible = false;
-var contactIsVisible = false;
-var diameterCircle = 200;
-
-// ------------------------------------------------------------------------------------------------------------
-
-var keywords=['info', 'idealism']; //,'idealism1','capitalism2','body3','xeno4'
-var keywordsURL=['www.anaconda.cc/reality', 'www.anaconda.cc/idealism', 'www.anaconda.cc/capitalism', 'www.anaconda.cc/body', 'www.anaconda.cc/xeno']
-var projectlist = ['The Great Encounter, The Ending', 'The Young Planet', '04PP', 'Shush Tones', 'Berlin Netzkunst', 'Next Project', 'TongueTongue', 'The Great Encounter, The Ending', 'The Young Planet', '04PP', 'Shush Tones', 'Berlin Netzkunst', 'Next Project', 'TongueTongue']
-
-var $wrapper;
-var $div;
-var $p;
-var $a;
-var randomPosX;
-var randomPosY;
-var yyy;
-var initialPositionX;
-var initialPositionY;
-var circleArr = new Array();
-
-
-var startTime = +(new Date());
-var startTimeArr = new Array(5);
-
-var circle;
-
-var circleObjectArr = new Array(); // Object mit allen Instanzen
-var circleObjectArrPosY = new Array(); // aus Object herausdistillierte y-Werte
-var stepArr = new Array(5);
-let start = null;
-
-var initialX;
-var initialY;
-
-var circlePosUpdate;
-
-var slideIndex = 1;
-var closeSlideshow = false;
-var counter = 0;
-var j = 1;
-
-var currentParent;
-var currentParentHover;
-
-var exampleDestination = document.querySelector('.projecttitle1');
 
 
 
@@ -241,199 +243,34 @@ function enableScroll() {
     document.onkeydown = null;
 }
 
-// END ENABLE/DISABLE SCROLL
-
-$(".projecttitleContainer").click(function(event, j) {
-    event.stopPropagation();
-    // show gallery based on class name of parent of clicked element
-    currentParent = $(document).find(this).parent().parent().attr('class');
-    selectParent(currentParent);
-    $('.imageCount').css('opacity', '1').html('1');
-    disableScroll();
-    $( ".circle0" ).animate({ "top": "-=350px" }, 1000 ); // pusht circle rapide raus
-});
-
-
-
-function selectParent() {
-
-
-
-    switch (currentParent) {
-      case "clipProjecttitle1":
-        $('.slideshow-container').addClass('show').removeClass('hide');
-        console.log(currentParent);
-        j=1;
-        $('.slideshow-container div').not(document.getElementsByClassName( 'mySlides1' )).css('display', 'none');
-        $('.mySlides1:first').css('display', 'block');
-        $('.mySlides1:first').addClass('width2');
-        // smoothscroll center thumbnailimage
-        var $window = $(window),
-            $element = $('.imageThumbnail:eq(0)'),
-            elementTop = $element.offset().top,
-            elementHeight = $element.height(),
-            viewportHeight = $window.height(),
-            scrollIt = elementTop - ((viewportHeight - elementHeight) / 2);
-            smoothScroll(scrollIt, 800);
-        break;
-      case "clipProjecttitle2":
-        $('.slideshow-container').addClass('show').removeClass('hide');
-        console.log(currentParent);
-        j=2;
-        $('.slideshow-container div').not(document.getElementsByClassName( 'mySlides2' )).css('display', 'none');
-        $('.mySlides2:first').css('display', 'block');
-        $('.mySlides2:first').addClass('width2');
-        // smoothscroll center thumbnailimage
-        var $window = $(window),
-            $element = $('.imageThumbnail:eq(1)'),
-            elementTop = $element.offset().top,
-            elementHeight = $element.height(),
-            viewportHeight = $window.height(),
-            scrollIt = elementTop - ((viewportHeight - elementHeight) / 2);
-            smoothScroll(scrollIt, 800);
-        break;
-      case "clipProjecttitle3":
-        console.log("Bananas are $0.48 a pound.");
-        j=3;
-        break;
-    }
-    return j;
-
-
-}
-
-// showSlides(slideIndex); // zeigt erstes Bild an
+// function createProjectlist() {
 //
-// $('body').on('click','img',function(){
-//     plusSlides(1);
-//     // var filterVal = 'blur(5px)';
-//     // $('.projecttitle1')
-//     //   .css('filter',filterVal)
-//     //   .css('webkitFilter',filterVal)
-//     //   .css('mozFilter',filterVal)
-//     //   .css('oFilter',filterVal)
-//     //   .css('msFilter',filterVal);
+//   updateSpanWrapperWidthArr = new Array(projectlist.length);
 //
-//     event.stopPropagation();
-// })
+//   for(var i=0;i<projectlist.length;i++){
+//     updateSpanWrapperWidthArr[i]=0; //fill empty array with zeros
+//     var $spanClass = $(".wrapperSpans div div").eq( i ).attr('class', 'wrapperProject' + i);
+//     spanWrapperWidth = $('.wrapperProject' + i).width();
+//     initialSpanWrapperWidthArr.push(spanWrapperWidth);
 //
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
+//     while (updateSpanWrapperWidthArr[i] <= window.innerWidth){
+//         var $wrapperSpan = $('<span />').appendTo('.wrapperProject'+i);
+//         $wrapperSpan.attr('class', 'wrapperSpan');
+//         $wrapperSpan.text(projectlist[i]);
+//         updateSpanWrapperWidthArr[i] += initialSpanWrapperWidthArr[i];
+//     }
+//   }
+//   console.log("initialSpanWrapperWidthArr " + initialSpanWrapperWidthArr);
+//   console.log("window.innerWidth " + window.innerWidth);
+//   console.log("updateSpanWrapperWidthArr " + updateSpanWrapperWidthArr);
 // }
-//
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-//
-// function showSlides(n) {
-//     counter++;
-//     var width1 = 1;
-//     var width2 = 2;
-//     var width3 = 3;
-//     var randomImageWidth = Math.random() < 0.5 ? width1 : (Math.random() < 0.5 ? width2 : width3);
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides" + j);
-//
-//   if (n > slides.length) {
-//       slideIndex = 1;
-//   }
-//   if (n < 1) {
-//       slideIndex = slides.length;
-//
-//   } // rückwärts klicken >> index springt auf das letzte Bild
-//   if (counter > slides.length) { // when last image in slideshow is reached
-//        $('.slideshow-container').addClass('hide').removeClass('show');
-//        $('.imageCount').css('opacity', '0');
-//        enableScroll();
-//        slides.className = "mySlides" + j + " fade";
-//        closeSlideshow = true;
-//        counter = 1;
-//   }
-//   for (i = 0; i < slides.length; i++) { // alle Slides werden versteckt...
-//       slides[i].style.display = "none";
-//       slides[i].className = "mySlides" + j + " fade";
-//   }
-//   if (1 < n && n <= slides.length) { // imageCounter
-//       $('.imageCount').css('opacity', '1').html(slideIndex);
-//   }
-//   // SHOW CURRENT SLIDE
-//   slides[slideIndex-1].style.display = "block"; // ... um dann das aktuelle bild anzuzeigen
-//   if (!(n==slides.length)) { // so that width isn't applied to last text slide
-//   console.log("n" + n);
-//       // $('.slideshow-container>div').removeClass("div[class*='width']");
-//       slides[slideIndex-1].className += " width" + randomImageWidth; // fügt eine von 3 Breiten hinzu
-//   }
-//
-// }
-
-// random width von projecttitle entfernt
-
-$(document).click(function() { // close Slideshow
-    if (closeSlideshow==true) { // closes slideshow after last slide
-        $('.slideshow-container').addClass('hide').removeClass('show');
-        $("div[class*='width']").removeClass (function (index, css) {
-           return (css.match (/(^|\s)width\S+/g) || []).join(' ');
-        });
-        closeSlideshow = false;
-    }
-    $('.slideshow-container').addClass('hide').removeClass('show');
-    $('.slideshow-container>div').css('display', 'none');
-    $('.imageCount').css('opacity', '0').
-    n = 1;
-    slideIndex=1;
-    closeSlideshow = false;
-    counter = 1;
-    enableScroll();
-});
-
-
-
-var initialSpanWrapperWidthArr = new Array();
-var spanWrapperWidth;
-var updateSpanWrapperWidthArr;
-
-var element = document.body;
-
-console.log(window.innerHeight);
-
-function myScroll() {
-  // var elem = $('body,html').scrollTop();
-  var elmnt = document.body;
-    var x = elmnt.scrollLeft;
-    var y = elmnt.scrollTop;
-    console.log(y);
-}
-
-myScroll();
-
-function createProjectlist() {
-
-  updateSpanWrapperWidthArr = new Array(projectlist.length);
-
-  for(var i=0;i<projectlist.length;i++){
-    updateSpanWrapperWidthArr[i]=0; //fill empty array with zeros
-    var $spanClass = $(".wrapperSpans div div").eq( i ).attr('class', 'wrapperProject' + i);
-    spanWrapperWidth = $('.wrapperProject' + i).width();
-    initialSpanWrapperWidthArr.push(spanWrapperWidth);
-
-    while (updateSpanWrapperWidthArr[i] <= window.innerWidth){
-        var $wrapperSpan = $('<span />').appendTo('.wrapperProject'+i);
-        $wrapperSpan.attr('class', 'wrapperSpan');
-        $wrapperSpan.text(projectlist[i]);
-        updateSpanWrapperWidthArr[i] += initialSpanWrapperWidthArr[i];
-    }
-  }
-  console.log("initialSpanWrapperWidthArr " + initialSpanWrapperWidthArr);
-  console.log("window.innerWidth " + window.innerWidth);
-  console.log("updateSpanWrapperWidthArr " + updateSpanWrapperWidthArr);
-}
 
 // ----------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------- CIRCLES START
 
 
-class KeywordCircle {
+class KeywordCircle { //create new KeywordCircle Object
     constructor(initialX,initialY){
         this.initialX = initialX;
         this.initialY = initialY;
@@ -455,14 +292,14 @@ function generateCirclePosition() { // 1.1
 }
 
 function createInitialCircles() { // 1.2
-    for(var i=0;i<keywords.length;i++){ //generates 5 initial circles
+    for(var i=0;i<keywords.length;i++){ //create empty circle objects
         generateCirclePosition();
         circle = new KeywordCircle(initialX,initialY);
         circleObjectArr.push(circle); // Array mit allen KeywordCircle Instanzen
-        circleObjectArrPosY.push(circleObjectArr[i].initialY); // Array mit allen Keyword Instanzen y-Werten
+        // circleObjectArrPosY.push(circleObjectArr[i].initialY); // Array mit allen Keyword Instanzen y-Werten RAUS?
+        console.log("i: " + i);
+        console.log(circle);
     }
-    // console.log("circleObjectArr: " + circleObjectArr);
-    console.log("circleObjectArrPosY: " + circleObjectArrPosY);
 }
 
 
@@ -523,7 +360,7 @@ function updateCirclePos() {
 
 function drawFirstCircles() { // 2
   // createInitialCircles(); //raus und stattdessen in document.ready function drinlassen?
-    for(var i=0;i<5;i++){
+    for(var i=0;i<keywords.length;i++){ // fill empty circle objects
         $wrapper = $('<div />').appendTo('body');
         $wrapper.attr('class', 'circleAnimation');
         $div = $('<div />').appendTo($wrapper);
@@ -531,22 +368,19 @@ function drawFirstCircles() { // 2
         $a = $('<a />').appendTo($p);
         $a.attr('href', keywordsURL[i]);
         $a.text( keywords[i] );
-        $p.attr('class', 'textKeywords');
         $div.attr('class', 'circle'+ i);
+        $p.attr('class', 'textKeywords');
         // rotation
         var rotation1 = 1;
         var rotation2 = 2;
         var rotation3 = 3;
         var randomIdCircle = Math.random() < 0.5 ? rotation1 : (Math.random() < 0.5 ? rotation2 : rotation3);
         $div.attr('id', 'rotationCircle'+ randomIdCircle);
-        // position (kann später mit updateCircles raus?)
+        // position
         $(".circle" + i).css("left", circleObjectArr[i].initialX);
         $(".circle" + i).css("--keywordcircle-top", circleObjectArr[i].initialY);
+        console.log("circleObjectArr " + circleObjectArr[i]);
     }
-
-    $('.aboutIcon').css("left", initialX);
-    $('.aboutIcon').css("top", initialY);
-    $('.aboutIcon').attr('id', 'rotationCircle2');
 
     // updateCirclePos(); //raus und stattdessen in document.ready function drinlassen?
   }
@@ -555,7 +389,7 @@ function drawFirstCircles() { // 2
 
 
  $(document).ready(function(){
-     createProjectlist();
+     // createProjectlist();
      // DRAW CIRCLES
      createInitialCircles();
      drawFirstCircles();
@@ -570,17 +404,14 @@ function drawFirstCircles() { // 2
          $(".clipProjecttitle1").css('--height-clip', imageHeight);
          $(".uno").css('--height-clip', imageHeight);
          $(".clipPath1").css('--height-clip', imageHeight);
-         console.log("innerHeight = " + imageHeight);
 
          var imageHeight2 = $(".imageThumbnail:eq(1)").innerHeight();
          $(".clipProjecttitle2").css('--height-clip2', imageHeight2);
          $(".due").css('--height-clip2', imageHeight2);
-         console.log("innerHeight2 = " + imageHeight2);
 
          var imageHeight3 = $(".imageThumbnail:eq(2)").innerHeight();
          $(".clipProjecttitle3").css('--height-clip3', imageHeight3);
          $(".tres").css('--height-clip3', imageHeight3);
-         console.log("innerHeight3 = " + imageHeight3);
     });
 
 
